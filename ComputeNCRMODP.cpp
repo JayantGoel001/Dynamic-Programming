eea8c9147d2435d29fc5bb16bfaed2d2a488a0d4
CormacKrum <54479676+CormacKrum@users.noreply.github.com>
2020-05-15 21:26:24 +0530

    Add files via upload

---
+++ ComputeNCRMODP.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int binoCoff(int n,int r,int p)
+{
+    if(r>n-r)
+    {
+        r=n-r;
+    }
+    int *dp=new int[r+1];
+    for(int i=0;i<=r;i++)
+    {
+        dp[i]=0;
+    }
+    dp[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=min(i,r);j>0;j--)
+        {
+            dp[j]=(dp[j]+dp[j-1])%p;
+        }
+    }
+    return dp[r];
+}
+int main()
+{
+    int n,r;
+    cout<<"Enter the value of n,r and p :\n";
+    cin>>n;
+    cin>>r;
+    int p;
+    cin>>p;
+    cout<<"The NCR mod p using DP of C("<<n<<","<<r<<") is : "<<binoCoff(n,r,p)<<endl;
+}
