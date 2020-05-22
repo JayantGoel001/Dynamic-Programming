7c44ff9dc77742891a9ca055b4b6ca3847a24c05
CormacKrum <54479676+CormacKrum@users.noreply.github.com>
2020-05-22 06:30:36 +0530

    Add files via upload

--- 0-1KnapSackProblem.cpp
+++ 0-1KnapSackProblem.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 //Time Complexity is O(2^n) And Space Complexity is O(n) in stack
 int knapsackRec(int *value,int *weight,int n,int W)
@@ -14,7 +15,7 @@ int knapsackRec(int *value,int *weight,int n,int W)
     else
     {
         return max(knapsackRec(value,weight,n-1,W),
-                   knapsackRec(value,weight,n-1,W-weight[n-1])+value[n-1]);
+        knapsackRec(value,weight,n-1,W-weight[n-1])+value[n-1]);
     }
 }
 //Time Complexity is O(n*W) And Space Complexity is O(n*w)
@@ -50,6 +51,23 @@ int knapsackDP(int *value,int *weight,int n,int W)
             }
         }
     }
+    cout<<"The Values Included in 0/1 Knapsack are :\n";
+    int total=dp[n][W];
+    int w=W;
+    for(int i=n;i>0 && total>0;i--)
+    {
+        if(total==dp[i-1][w])
+        {
+            continue;
+        }
+        else
+        {
+            cout<<weight[i-1]<<" ";
+            total-=value[i-1];
+            w-=weight[i-1];
+        }
+    }
+    cout<<endl;
     return dp[n][W];
 }
 int main()
