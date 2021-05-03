98933a5613f632ba630752495e0ea11d458afac8
JayantGoel001 <jgoel92@gmail.com>
2021-05-03 16:48:42 +0530

    Initial Commit

--- howSum.cpp
+++ howSum.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <map>
+#include <vector>
 using namespace std;
-bool canSum(int *ar,int n,int sum,map<int,bool> &mp){
+vector<int> howSum(int *ar,int n,int sum,map<int,vector<int>> &mp){
     if (sum==0){
-        return true;
+        vector<int> v;
+        v.push_back(0);
+        return v;
     }
     if (sum<0){
-        return false;
+        return vector<int>();
     }
     if (mp.count(sum)!=0){
         return mp[sum];
     }
     for (int i = 0; i < n; ++i) {
         int rem = sum-ar[i];
-        mp[rem] = canSum(ar,n,rem,mp);
-        if (mp[rem]){
+        mp[rem] = howSum(ar,n,rem,mp);
+        if (!mp[rem].empty()){
+            mp[rem].push_back(ar[i]);
             return mp[rem];
         }
     }
-    mp[sum] = false;
+    mp[sum] = vector<int>();
     return mp[sum];
 }
+
 int main(){
     cout<<"Enter The value of n:\n";
     int n;
@@ -33,10 +38,13 @@ int main(){
     cout<<"Enter The value of sum:\n";
     int sum;
     cin>>sum;
-    map<int,bool> mp;
-    if (canSum(ar,n,sum,mp)) {
-        cout << "YES";
+    map<int,vector<int>> mp;
+    vector<int> v =howSum(ar,n,sum,mp);
+    if (!v.empty()) {
+        for (int i = 1; i < v.size(); ++i) {
+            cout<<v[i]<<" ";
+        }
     } else{
-        cout<<"NO";
+        cout<<"Not Possible";
     }
 }
