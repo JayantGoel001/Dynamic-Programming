5af694fe883d9212d8e0eb8c909a0c33c0784269
JayantGoel001 <jgoel92@gmail.com>
2021-05-14 19:43:24 +0530

    Initial Commit

--- FibonacciNumber.cpp
+++ FibonacciNumber.cpp
@@ -1,21 +1,35 @@
 #include<iostream>
+#include <map>
 using namespace std;
 //Time Complexity O(n) And Space Complexity O(n)
-int fibo(int n)
-{
+int fibonacci(int n){
     int *ar=new int[n+1];
     ar[0]=0;
     ar[1]=1;
-    for(int i=2;i<=n;i++)
-    {
+    for(int i=2;i<=n;i++){
         ar[i]=ar[i-1]+ar[i-2];
     }
     return ar[n];
 }
-int main()
-{
+// Time Complexity is O(n) and Space Complexity is O(n)
+int fibonacci2(int n, map<int, int> &mp){
+    if (mp.count(n)!=0){
+        return mp[n];
+    }
+    if (n==1 || n==0){
+        mp[n] = n;
+        return mp[n];
+    }
+    mp[n] = fibonacci2(n-1,mp) + fibonacci2(n-2,mp);
+    return mp[n];
+}
+int main(){
     int n;
     cout<<"Enter the value of n:\n";
     cin>>n;
-    cout<<"Fibonacci Number at "<<n<<" is :"<<fibo(n);
+    cout<<"Using Bottom Up Approach:\n";
+    cout<<"Fibonacci Number at "<<n<<" is :"<<fibonacci(n)<<"\n";
+    map<int,int> mp;
+    cout<<"Using Top Down Approach:\n";
+    cout<<"Fibonacci Number at "<<n<<" is :"<<fibonacci2(n,mp);
 }
--- GridTraveler.cpp
+++ GridTraveler.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #define ll long long int
 using namespace std;
+// Time Complexity is O(nm) and Space Complexity is O(mn).
 ll solveGridTraveler(int n,int m,map<pair<int,int>,ll> &mp){
     if (n==0 || m==0){
         return 0;
@@ -20,10 +21,34 @@ ll solveGridTraveler(int n,int m,map<pair<int,int>,ll> &mp){
 
     return mp[make_pair(n,m)];
 }
+// Time Complexity is O(nm) and Space Complexity is O(mn).
+ll solveGridTraveler2(int n,int m){
+    int dp[n+1][m+1];
+    for (int i = 0; i <=n; ++i) {
+        for (int j = 0; j <=m; ++j) {
+            dp[i][j] = 0;
+        }
+    }
+    dp[1][1]= 1;
+    for (int i = 1; i <=n ; ++i) {
+        for (int j = 1; j <=m; ++j) {
+            if (i==1 || j==1){
+                dp[i][j]=1;
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+    }
+    return dp[n][m];
+}
 int main(){
     cout<<"Enter the value of n and m:\n";
     int n,m;
     cin>>n>>m;
     map<pair<int,int>,ll> mp;
-    cout<<solveGridTraveler(n,m,mp);
+    cout<<"Using Top Down Approach:\n";
+    cout<<"Fibonacci Number at "<<n<<" is : "<<solveGridTraveler(n,m,mp)<<'\n';
+
+    cout<<"Using Bottom Up Approach:\n";
+    cout<<"Fibonacci Number at "<<n<<" is : "<<solveGridTraveler2(n,m)<<"\n";
 }
