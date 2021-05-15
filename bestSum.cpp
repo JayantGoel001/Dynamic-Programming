c142fc1ddcafeaad4c1367fc969471e427a1842c
JayantGoel001 <jgoel92@gmail.com>
2021-05-15 16:49:38 +0530

    Initial Commit

--- bestSum.cpp
+++ bestSum.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 using namespace std;
+// Time Complexity is O(n*m^2) And Space Complexity is O(m*m).
 vector<int> bestSum(int *ar,int n,int sum,map<int,vector<int>> &mp){
     if (sum==0){
         vector<int> v;
@@ -29,7 +30,20 @@ vector<int> bestSum(int *ar,int n,int sum,map<int,vector<int>> &mp){
     mp[sum] = minLength;
     return mp[sum];
 }
-
+// Time Complexity is O(n*m^2) And Space Complexity is O(m*m).
+vector<int> bestSum2(const int *ar,int n,int sum){
+    vector<int> *dp = new vector<int>[sum+1];
+    dp[0].push_back(0);
+    for (int i = 1; i <=sum; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i>=ar[j] && !dp[i-ar[j]].empty() && (dp[i].empty() || dp[i].size()>dp[i-ar[j]].size()+1)){
+                dp[i] = dp[i-ar[j]];
+                dp[i].push_back(ar[j]);
+            }
+        }
+    }
+    return dp[sum];
+}
 int main(){
     cout<<"Enter The value of n:\n";
     int n;
@@ -44,6 +58,16 @@ int main(){
     cin>>sum;
     map<int,vector<int>> mp;
     vector<int> v = bestSum(ar,n,sum,mp);
+    cout<<"Using Top Down Approach :\n";
+    if (!v.empty()) {
+        for (int i = 1; i < v.size(); ++i) {
+            cout<<v[i]<<" ";
+        }
+    } else{
+        cout<<"NO";
+    }
+    cout<<"\nUsing Bottom Up Approach:\n";
+    v = bestSum2(ar,n,sum);
     if (!v.empty()) {
         for (int i = 1; i < v.size(); ++i) {
             cout<<v[i]<<" ";
