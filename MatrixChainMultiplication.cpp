cbe1dc64bb65fa1633dc77bb80b21fe6f735e879
JayantGoel001 <jgoel92@gmail.com>
2021-05-18 14:02:51 +0530

    Initial Commit

--- MatrixChainMultiplication.cpp
+++ MatrixChainMultiplication.cpp
@@ -1,28 +1,22 @@
 #include<iostream>
 using namespace std;
 //Time Complexity is O(2^n) And Space Complexity is O(n) in stack
-int matrixChainOrder(int *mat,int i,int j)
-{
-    if(i==j)
-    {
+int matrixChainOrder(int *mat,int i,int j){
+    if(i==j){
         return 0;
     }
     int mini=INT_MAX;
     int x;
-    for(int k=i;k<j;k++)
-    {
-        x=matrixChainOrder(mat,i,k)+matrixChainOrder(mat,k+1,j)+mat[i-1]*mat[k]*mat[j];
-        if(x<mini)
-        {
+    for(int k=i;k<j;k++){
+        x = matrixChainOrder(mat,i,k)+matrixChainOrder(mat,k+1,j)+mat[i-1]*mat[k]*mat[j];
+        if(x<mini){
             mini=x;
         }
     }
     return mini;
 }
-void printPara(int i,int j,int **kar,char &name)
-{
-    if(i==j)
-    {
+void printPara(int i,int j,int **kar,char &name){
+    if(i==j){
         cout<<name++;
         return;
     }
@@ -31,10 +25,8 @@ void printPara(int i,int j,int **kar,char &name)
     printPara(kar[i][j]+1,j,kar,name);
     cout<<")";
 }
-void printPara2(int i,int j,int **kar,char &name)
-{
-    if(i==j)
-    {
+void printPara2(int i,int j,int **kar,char &name){
+    if(i==j){
         cout<<name++;
         return;
     }
@@ -44,34 +36,26 @@ void printPara2(int i,int j,int **kar,char &name)
     cout<<")";
 }
 //Time Complexity is O(n^3) And Space Complexity is O(n^2)
-int matrixChainOrderDP(int *mat,int n)
-{
+int matrixChainOrderDP(int *mat,int n){
     int **m=new int*[n];
     int **kar=new int*[n];
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         m[i]=new int[n];
         kar[i]=new int[n];
     }
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             m[i][j]=0;
             kar[i][j]=0;
         }
     }
-    for(int l=1;l<n-1;l++)
-    {
-        for(int i=1;i<n-l;i++)
-        {
+    for(int l=1;l<n-1;l++){
+        for(int i=1;i<n-l;i++){
             int j=i+l;
             m[i][j]=INT_MAX;
-            for(int k=i;k<j;k++)
-            {
+            for(int k=i;k<j;k++){
                 int x=m[i][k]+m[k+1][j]+mat[i-1]*mat[k]*mat[j];
-                if(x<m[i][j])
-                {
+                if(x<m[i][j]){
                     m[i][j]=x;
                     kar[i][j]=k;
                     //another approach
@@ -93,15 +77,13 @@ int matrixChainOrderDP(int *mat,int n)
     //another approach
     return m[1][n-1];
 }
-int main()
-{
+int main(){
     int n;
     cout<<"Enter the value of n:\n";
     cin>>n;
     int *mat=new int[n];
     cout<<"Enter the value of array:\n";
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         cin>>mat[i];
     }
     cout<<"Minimum number of multiplication Using Recurrence are ="<<matrixChainOrder(mat,1,n-1)<<endl;
